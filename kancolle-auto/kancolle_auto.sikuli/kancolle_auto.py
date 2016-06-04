18f03fd8555acdc9deb2824c706ca72fda1b23c2
BeemoLin <Beemo@BeemoMacBookAir.local>
2016-06-04 15:40:07 +0900

    fix combat change sub

--- kancolle-auto/kancolle_auto.sikuli/kancolle_auto.py
+++ kancolle-auto/kancolle_auto.sikuli/kancolle_auto.py
@@ -213,6 +213,10 @@ def sortie_action():
         go_home()
         if combat_item.count_damage_above_limit('repair') > 0:
             combat_item.go_repair()
+        #BeemoLin
+        if settings['submarine_switch']:
+            combat_item.switch_sub()
+            log_msg("Attempting to switch out submarines!")
         resupply()
         fleet_needs_resupply[0] = False
         if settings['combined_fleet']:
